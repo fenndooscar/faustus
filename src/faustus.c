9fe09f0e71693a22ef4d03aae65d324786051910
Игорь Тихомиров <52701857+fenndooscar@users.noreply.github.com>
2022-04-28 13:28:38 +0300

    Add support for FX506HL

--- src/faustus.c
+++ src/faustus.c
@@ -3431,6 +3431,14 @@ static const struct dmi_system_id atw_dmi_list[] __initconst = {
 			DMI_MATCH(DMI_PRODUCT_NAME, "FX506LI"),
 		},
 	},
+	{
+		.callback = dmi_check_callback,
+		.ident = "FA506IH",
+		.matches = {
+			DMI_MATCH(DMI_SYS_VENDOR, "ASUSTeK COMPUTER INC."),
+			DMI_MATCH(DMI_PRODUCT_NAME, "FA506IH"),
+		},
+	},
 	{}
 };
 
